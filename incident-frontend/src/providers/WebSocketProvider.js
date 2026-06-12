1c85ca48781e94af989f597c6a82a749b1eca7a1
Ilmir <ilmir@example.com>
2026-06-12 23:04:01 +0500

    updated recent activity with websockets

--- incident-frontend/src/providers/WebSocketProvider.js
+++ incident-frontend/src/providers/WebSocketProvider.js
@@ -5,6 +5,7 @@ import {
 } from "react";
 
 import { useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const WebSocketContext = createContext(null);
 
@@ -12,70 +13,115 @@ export const WebSocketProvider = ({ children }) => {
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    const socket = new WebSocket(
-      "ws://localhost:8000/incidents/ws"
-    );
 
-    socket.onopen = () => {
-      console.log("WS connected");
+    let socket = null;
+    let reconnectTimer = null;
+
+    const connect = () => {
+
+        socket = new WebSocket(
+        "ws://localhost:8000/incidents/ws"
+        );
+
+        socket.onopen = () => {
+        console.log("WS connected");
+
+        toast.success(
+            "Realtime подключен"
+        );
+        };
+
+        socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        console.log("WS event:", data);
+
+        switch (data.type) {
+
+            case "incident_created":
+
+            toast.success(
+                "Создан новый инцидент"
+            );
+
+            queryClient.invalidateQueries({
+                queryKey: ["incidents"]
+            });
+
+            queryClient.invalidateQueries({
+                queryKey: ["dashboard"]
+            });
+
+            break;
+
+            case "incident_updated":
+
+            toast(
+                "Статус инцидента обновлен"
+            );
+
+            queryClient.invalidateQueries({
+                queryKey: ["incidents"]
+            });
+
+            queryClient.invalidateQueries({
+                queryKey: ["dashboard"]
+            });
+
+            break;
+
+            case "incident_deleted":
+
+            toast.error(
+                "Инцидент удален"
+            );
+
+            queryClient.invalidateQueries({
+                queryKey: ["incidents"]
+            });
+
+            queryClient.invalidateQueries({
+                queryKey: ["dashboard"]
+            });
+
+            break;
+
+            default:
+            break;
+        }
+        };
+
+        socket.onclose = () => {
+
+        console.log(
+            "WS disconnected"
+        );
+
+        reconnectTimer = setTimeout(
+            () => {
+            connect();
+            },
+            5000
+        );
+        };
     };
 
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-
-      console.log("WS event:", data);
-
-      switch (data.type) {
-
-        case "incident_created":
-
-          queryClient.invalidateQueries({
-            queryKey: ["incidents"]
-          });
-
-          queryClient.invalidateQueries({
-            queryKey: ["dashboard"]
-          });
-
-          break;
-
-        case "incident_updated":
-
-          queryClient.invalidateQueries({
-            queryKey: ["incidents"]
-          });
-
-          queryClient.invalidateQueries({
-            queryKey: ["dashboard"]
-          });
-
-          break;
-
-        case "incident_deleted":
-
-          queryClient.invalidateQueries({
-            queryKey: ["incidents"]
-          });
-
-          queryClient.invalidateQueries({
-            queryKey: ["dashboard"]
-          });
-
-          break;
-
-        default:
-          break;
-      }
-    };
-
-    socket.onclose = () => {
-      console.log("WS disconnected");
-    };
+    connect();
 
     return () => {
-      socket.close();
+
+        if (reconnectTimer) {
+        clearTimeout(
+            reconnectTimer
+        );
+        }
+
+        if (socket) {
+        socket.close();
+        }
     };
-  }, [queryClient]);
+
+    }, [queryClient]);
 
   return (
     <WebSocketContext.Provider value={null}>
